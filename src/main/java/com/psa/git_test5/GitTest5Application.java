af85eff32978022209d3725f931ada8a157a388b
rathoursumit <rathoursumit648@gmail.com>
2025-04-09 20:08:10 +0530

    Update GitTest5Application.java

--- src/main/java/com/psa/git_test5/GitTest5Application.java
+++ src/main/java/com/psa/git_test5/GitTest5Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitTest5Application {
          int y=300;
         int x=200;
+	int s=382;
 	public static void main(String[] args) {
 		SpringApplication.run(GitTest5Application.class, args);
 	}
