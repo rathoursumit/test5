bf9d2a74ad404b374e8c9f78f735909f00ec5535
rathoursumit <rathoursumit@gmail.com>
2025-04-09 19:06:44 +0530

    comitt to chek

---
+++ src/main/java/com/psa/git_test5/GitTest5Application.java
@@ -0,0 +1,13 @@
+package com.psa.git_test5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTest5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTest5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/psa/git_test5/GitTest5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psa.git_test5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTest5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
